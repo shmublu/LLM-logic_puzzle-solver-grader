27abb3a76735b4e88dfd6f437c09649cf2348242
Shmuel Berman <shmuelaberman@gmail.com>
2023-12-03 15:37:15 -0500

    changed role text, added normal LLM

--- LLM-solver-grader.py
+++ LLM-solver-grader.py
@@ -3,6 +3,21 @@ import subprocess
 import tempfile
 import os
 from openai import OpenAI
+import datetime
+import csv
+
+class NaiveSolver:
+    def __init__(self, LLMapi, example=None):
+        self.example = example
+        self.LLMapi = LLMapi
+        self.conversation = [] if not self.example else [self.example, ""]
+    def solve_puzzle(self, prompt):
+        self.conversation.append(prompt)
+        response = self.LLMapi.get_response(self.conversation)
+        self.conversation.append(response)
+        return response
+    def clear(self):
+        self.conversation = [] if not self.example else [self.example, ""]
 
 class PuzzleSolver:
     def __init__(self, LLMapi, example=None):
@@ -127,43 +142,102 @@ class LLMApi:
         return structured_history
 
 
-solver_llm = LLMApi(role="Generate SMT-LIB code that encodes each fact, whether implicit or explicit, in the following logic puzzle, one by one. After you respond, you will receive the output of your SMT-LIB code in z3; use it to correct your mistakes in coding. syntax and encoding the puzzle constraints. Make sure to set the logic and check-sat and get-model. Write 'I am done.' in your response, along with the SMT code, when you are sure of your response and it is free of errors.")
-grader_llm = LLMApi(role="Based on the answer to be graded, the SMT-LIB solver output use the answer key to grade it numerically in the form X/Y, where X is the number of correct assignments(from the answer to be graded) and Y is the total number of assignments(counted from the answer key). Please give your thought process as well in calculating both X and Y.")
-solver = PuzzleSolver(solver_llm)
-grader = SolverGrader(grader_llm)
+class PuzzleData:
+    def __init__(self, answers, entities, clues):
+        self.answers = answers
+        self.entities = entities
+        self.clues = clues
+
+def read_file_contents(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def process_puzzles(directory_path):
+    import os
+
+    puzzles = []
+
+    for folder in os.listdir(directory_path):
+        folder_path = os.path.join(directory_path, folder)
+
+        if not os.path.isdir(folder_path):
+            continue
+
+        # Check for required files
+        answers_path = os.path.join(folder_path, 'answers.txt')
+        entities_path = os.path.join(folder_path, 'entities.txt')
+        clues_path = os.path.join(folder_path, 'clues.txt')
+
+        if os.path.exists(answers_path) and os.path.exists(entities_path) and os.path.exists(clues_path):
+            answers = read_file_contents(answers_path)
+            entities = read_file_contents(entities_path)
+            clues = read_file_contents(clues_path)
+
+            puzzles.append(PuzzleData(answers, entities, clues))
+
+    return puzzles
 
 
-puzzle_description = '''customers, cake shapes, delivery dates
 
-Becker, Ingram, Keller, Massey
-bowling pin, rocket ship, sailboat, turtle
-October 5, October 6, October 7, October 8
-The October 8 delivery will be in the shape of a sailboat.
-The order shaped like a bowling pin will be delivered sometime before Mrs. Keller's cake.
-Mrs. Massey's cake will be delivered on October 8.
-The order shaped like a turtle is either Mrs. Becker's order or Mrs. Massey's cake.
-Mrs. Becker's cake will be delivered 1 day after the order shaped like a rocket ship.'''
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+csv_file_name = f'LLM-SMT_log_{timestamp}.csv'
+csv_file = open(csv_file_name, 'w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Puzzle', 'SMT-LIB Code', 'Attempted Solution', 'Full LLM Convo' , 'Grading Process', 'Grade', 'Solution'])
 
-# Use LLMApi to generate SMT-LIB code from the puzzle description
-next_input = puzzle_description
-max_conversation_length = 6
-for i in range(max_conversation_length):
-    full_response, smt_lib_code = solver.solve_puzzle(next_input)
-    if 'I am done.' in full_response:
-        break
-    next_input = solver.solve_with_z3(smt_lib_code)
-    print(full_response, next_input)
+puzzles = process_puzzles("./data/puzzles")
+for puzzle in puzzles:
+    puzzle_description = puzzle.entities + "\n" + puzzle.clues
+    solution = puzzle.answers
+    print(puzzle_description)
+    solver_role_text = (
+    "Role: Encode logic puzzles into SMT-LIB code, considering all explicit and implicit facts. "
+    "Submit this code to the z3 solver and analyze the output. If the solver returns an error, "
+    "identify and correct any syntactical errors or constraint misinterpretations in your code. "
+    "Iteratively refine the code until the solver returns a valid solution that aligns with the puzzle's options. "
+    "If the solver's solution does not align with the original puzzle's options, reassess and refine the constraints to ensure they accurately reflect the puzzle's logic. "
+    "Only after submitting the SMT-LIB code to the z3 solver and receiving a model that you believe correctly solves the puzzle, "
+    "conclude your response with 'I am done.' along with the final, error-free SMT-LIB code."
+    )
 
-print(full_response, smt_lib_code)
-# Solve the puzzle using Z3
-attempted_solution = solver.solve_with_z3(smt_lib_code)
-solution = '''Becker, turtle, October 7
-Ingram, bowling pin, October 5
-Keller, rocket ship, October 6
-Massey, sailboat, October 8 '''
-full_response, grade = grader.get_grade(solution, attempted_solution, full_response)
+    grader_role_text = (
+    "Role: Grade SMT-LIB solver outputs numerically. Use the answer key and the latest solver output "
+    "to determine the score in the format X/Y. 'X' represents the number of correct assignments in the "
+    "given answer; attempt to interpret the solution and find X even if the SMT model contains errors. 'Y' is the total number of assignments as per "
+    "the answer key. Provide a detailed explanation of your thought process in calculating both X and Y."
+    )
 
-print("SMT-LIB Code:\n", smt_lib_code)
-print("Solution:\n", attempted_solution)
-print("Grading Process: ", full_response)
-print("Grade: ", grade)
+    solver_llm = LLMApi(role=solver_role_text)
+    grader_llm = LLMApi(role=grader_role_text)
+    solver = PuzzleSolver(solver_llm)
+    grader = SolverGrader(grader_llm)
+
+    # Use LLMApi to generate SMT-LIB code from the puzzle description
+    next_input = puzzle_description
+    max_conversation_length = 6
+    latest_smt_code = ""
+    for i in range(max_conversation_length):
+        full_response, smt_lib_code = solver.solve_puzzle(next_input)
+        if smt_lib_code and "(set-logic" in smt_lib_code:
+            latest_smt_code = smt_lib_code
+        if 'I am done.' in full_response:
+            break
+        next_input = solver.solve_with_z3(latest_smt_code)
+
+    # Solve the puzzle using Z3
+    attempted_solution = solver.solve_with_z3(latest_smt_code)
+    #print(full_response, latest_smt_code, attempted_solution)
+
+    grading_full_response, grade = grader.get_grade(solution, full_response, attempted_solution)
+    csv_writer.writerow([puzzle_description, latest_smt_code, attempted_solution, full_response,grading_full_response, grade, solution])
+    print("SMT-LIB Code:\n", latest_smt_code)
+    print("Solution:\n", attempted_solution)
+    print("Grading Process: ", grading_full_response)
+    print("Grade: ", grade)
+
+"""
+baseline LLM
+vs us
+
+benchmark the grader
+"""
