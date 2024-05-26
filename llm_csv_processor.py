bcdc793d0d7c5230b1a95c4c15f45071ccaecd5c
Shmuel Berman <shmuelaberman@gmail.com>
2024-05-25 23:17:22 -0400

    made code actually usable

--- llm_csv_processor.py
+++ llm_csv_processor.py
@@ -9,15 +9,15 @@ def calculate_average(csv_file_path):
     with open(csv_file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            if len(row) >= 5:  # Check if the 5th column exists
+            if len(row) > 0:  
                 try:
-                    fraction = Fraction(row[-2])  # Convert 5th column to Fraction
+                    fraction = Fraction(row[0])  
                     if abs(fraction - 1) < 0.001:
                         how_many_perfect += 1
                     total += fraction
                     count += 1
                 except Exception:
-                    # Skip rows where the 5th column cannot be converted to a fraction
+                    
                     continue
 
     if count > 0:
@@ -28,9 +28,7 @@ def calculate_average(csv_file_path):
     else:
         return "No valid data"
 
-# Replace 'yourfile.csv' with your actual file path
-nodecomp_file = 'LLM-SMT-non-decomp_log_20240331_184935.csv'
-decomp_file = 'test-exp5-3.5-LLM_log_20240524_094700.csv'
-csv_file_path = decomp_file
+graded_file = 'AAtest-exp-log_20240525_231212.csv'
+csv_file_path = graded_file
 average = calculate_average(csv_file_path)
 print("Average:", average)
